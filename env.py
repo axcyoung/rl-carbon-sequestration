748f9ba7e4fc4f91beb9824f30c4a9df67f7e424
Qiuhong Wei <qiuhong.anna.wei@gmail.com>
2022-02-20 00:11:45 -0500

    fixed logging and reset reference

--- env.py
+++ env.py
@@ -12,7 +12,7 @@ from baselines.deepq.replay_buffer import ReplayBuffer
 from baselines.deepq.utils import ObservationInput
 from baselines.common.schedules import LinearSchedule
 
-
+import json
 import time
 
 # import warnings
@@ -120,7 +120,7 @@ class FCEnv(gym.Env):  # the forest carbon env
 
     def reset(self):
         """Returns state value in self.observation_space. Re-start the environment. """
-        self.state = self.initial_state
+        self.state = np.copy(self.initial_state)
         return self.state
 
     def step(self, action):
@@ -185,7 +185,154 @@ def eco_run(parameters, undisturbed_iter=1000, delta = 0.01):
         env.reset()
         env.eco_step(undisturbed_iter, delta)
 
+
+def dqn2(parameters, max_episode_num = 10, steps_per_episode=100):
+    """main difference in how things are looped. This one has max_episode_num and steps_per_episode.
+        returns 'act': ActWrapper
+        Wrapper over act function. Adds ability to save it and load it.
+        See header of baselines/deepq/categorical.py for details on the act function.
+     """
+    # https://github.com/openai/baselines/blob/master/baselines/deepq/experiments/custom_cartpole.py
+    with U.make_session(num_cpu=32):
+        env = FCEnv(**parameters) # Create the environment
+        # Create all the functions necessary to train the model
+        act, train, update_target, debug = deepq.build_train(
+            make_obs_ph=lambda name: ObservationInput(env.observation_space, name=name), # input placeholder for specific observation space
+            q_func=model,
+            num_actions=env.action_space.n,  # (1,)
+            optimizer=tf.train.AdamOptimizer(learning_rate=5e-4),
+            gamma = 0.99# relatively long-term
+        )
+        act_params = {
+            'make_obs_ph': lambda name: ObservationInput(env.observation_space, name=name),
+            'q_func': model,
+            'num_actions': env.action_space.n,
+        }
+        act = deepq.deepq.ActWrapper(act, act_params)
+
+        replay_buffer = ReplayBuffer(50000) # Create the replay buffer, Max number of transitions to store in the buffer
+        # Schedule for exploration: 1 (every action is random) -> 0.02 (98% of actions selected by values predicted by model)
+        # exploration = LinearSchedule(schedule_timesteps=5000, initial_p=1, final_p=1)
+        exploration_start = 0.9
+        exploration_end = 0.02
+        exploration_timestep = 2000
+        exploration_change = (exploration_end-exploration_start)/exploration_timestep
         
+
+        # Initialize the parameters and copy them to the target network.
+        U.initialize()
+        update_target()
+        exploration = exploration_start
+
+        total_stepct = 0
+        episode_rewards = [] # each ele = reward from one complete run until done
+        actions_taken = []
+
+        soil_carbons, tree_carbons, product_carbons, oldtree_cts, youngtree_cts = [], [], [], [], []
+        for episode in range(max_episode_num): # each episode: restart run, keep learning on old weights
+            state = env.reset()  # returns state
+            episode_rewards.append(0)
+            for step in range(steps_per_episode): # each step = 1 year, each episode = steps_per_episode years
+                if episode == max_episode_num-1:
+                    soil_carbon, tree_carbon, product_carbon, oldtree_ct, youngtree_ct = env.state 
+                    soil_carbons.append(soil_carbon)
+                    tree_carbons.append(tree_carbon)
+                    product_carbons.append(product_carbon)
+                    oldtree_cts.append(oldtree_ct)
+                    youngtree_cts.append(youngtree_ct)
+                total_stepct+=1
+                exploration = max(exploration + exploration_change, exploration_end)
+
+                env.eco_step(99, delta = 0.01)
+                if step%25==0: print(f"   [{episode}-{step}] preaction: state={env.state}, exploration={round(exploration, 5)}")
+                ## Pick action and update exploration to the newest value
+                # action = act(state[None], stochastic=False, update_eps=exploration.value(total_stepct))[0] # observation obj (axis added), stochastic boolean, update
+                action = act(state[None], stochastic=False)[0] if np.random.uniform(0,1) > exploration else env.action_space.sample()
+                new_state, rew, done, info = env.step(action) # updates state
+                actions_taken.append(action)
+                # if step%25==0: print(f"   \tpostaction: state={env.state}, action={action}")
+                ## Store transition in the replay buffer.
+                replay_buffer.add(state, action, rew, new_state, float(done))
+                state = new_state
+                episode_rewards[-1] += rew
+                if done: break # go to next episode
+                # is_solved = total_stepct > 100 and np.mean(episode_rewards[-101:-1]) >= 200
+                # if is_solved:
+                    # print(f"---SOLVED: {np.mean(episode_rewards[-101:-1])}---")
+                    # env.render()
+                else: 
+                    if total_stepct > -1: # Minimize the error in Bellman's equation on a batch sampled from replay buffer.
+                        obses_t, actions, rewards, obses_tp1, dones = replay_buffer.sample(32) # Sample a batch of experiences
+                        train(obses_t, actions, rewards, obses_tp1, dones, np.ones_like(rewards))
+            # end of each episode
+            update_target() # Update target network periodically.
+            logger.record_tabular("steps", total_stepct)
+            logger.record_tabular("episodes", len(episode_rewards))
+            logger.record_tabular("mean episode reward", round(np.mean(episode_rewards[-101:-1]), 1))
+            logger.record_tabular("% time spent exploring", int(exploration * 100)) #int(100 * exploration.value(total_stepct)
+            logger.dump_tabular()
+            print(f"### episode {episode} ### episode reward={episode_rewards[-1]} | Total carbon={env.total_carbon()}\n")
+
+        endtime = time.time()
+        print(soil_carbons)
+        with open(f"output/{endtime}_last_episode_vals.json", 'w') as f:
+            f.write(json.dumps({"soil_carbons":str(soil_carbons), "tree_carbons":str(tree_carbons), "product_carbons":str(product_carbons), 
+                "oldtree_cts":str(oldtree_cts), "youngtree_cts":str(youngtree_cts)}, indent=4))
+        episode_rewards = np.array(episode_rewards)/steps_per_episode
+        # act.save(f"output/{endtime}_act.pkl")
+        return endtime, actions_taken, episode_rewards, env.state
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    parameters = {
+        "initial_state": np.array([150, 76, 0, 328, 328], dtype=np.float32), # soil_carbon, tree_carbon, product_carbon, oldtree_ct, youngtree_ct
+        "K_T": 0.008, # tree litterfall rate
+        "K_S": 1/50, # soil decay
+        "K_P": 1/10, # product decay
+        "m_1": 1/37, # product decay
+        "d_t" : 656, # tree density
+        "NPP_y": 2.5, # carbon released tons/ha/year for young trees
+        "NPP_o": 0.2, # carbon released tons/ha/year for old trees
+        "r_o":0.6,
+        "r_y":0.2,
+        "C_o": 0.076, # 76
+        "C_y": 0.038, # 38
+        "death_o": 0.05, 
+        "death_y": 0.1
+    }
+
+    # eco_run(parameters, undisturbed_iter=2000)
+    # dqn1(parameters, max_step_ct = 1000)
+    endtime, actions_taken, episode_rewards, ending_state = dqn2(parameters, max_episode_num = 10, steps_per_episode=100)
+    print("\n------------------------ %s seconds ------------------------" % (endtime - start_time)) # round(time.time() - start_time, 2)
+    print(f"actions_taken={actions_taken}, Number of actions_taken={len(actions_taken)}")
+    print(f"episode_rewards={episode_rewards}")
+    print(f"ending_state={ending_state}")
+    print(parameters, "\n")
+    with open("log.txt", 'a') as file:
+        file.write(f"\n\n\n#####{endtime}#####\n")
+        file.writelines(str(parameters))
+        file.write("\n")
+        file.writelines(str(np.array(actions_taken[-300:])))
+        file.write("\n")
+        file.writelines(str(episode_rewards))
+        file.write("\n")
+        file.writelines(str(ending_state))
+
+        
+    
+    
+
+
+##### NOTES
+# act: function that choses an action given an observation
+# train: function that takes a transition (s,a,r,s') and optimizes Bellman equation's error:
+    # td_error = Q(s,a) - (r + gamma * max_a' Q(s', a'))
+    # loss = huber_loss[td_error]
+# https://github.com/openai/baselines/blob/ea25b9e8b234e6ee1bca43083f8f3cf974143998/baselines/deepq/build_graph.py#L317
+
+
 # def dqn1(parameters, max_step_ct=100):
 #     # https://github.com/openai/baselines/blob/master/baselines/deepq/experiments/custom_cartpole.py
 #     with U.make_session(num_cpu=32):
@@ -242,125 +389,3 @@ def eco_run(parameters, undisturbed_iter=1000, delta = 0.01):
 #                 logger.record_tabular("mean episode reward", round(np.mean(episode_rewards[-101:-1]), 1))
 #                 logger.record_tabular("% time spent exploring", int(100 * exploration.value(stepct)))
 #                 logger.dump_tabular()
-
-def dqn2(parameters, max_episode_num = 10, steps_per_episode=100):
-    """main difference in how things are looped. This one has max_episode_num and steps_per_episode.
-        returns 'act': ActWrapper
-        Wrapper over act function. Adds ability to save it and load it.
-        See header of baselines/deepq/categorical.py for details on the act function.
-     """
-    # https://github.com/openai/baselines/blob/master/baselines/deepq/experiments/custom_cartpole.py
-    with U.make_session(num_cpu=32):
-        env = FCEnv(**parameters) # Create the environment
-        # Create all the functions necessary to train the model
-        act, train, update_target, debug = deepq.build_train(
-            make_obs_ph=lambda name: ObservationInput(env.observation_space, name=name), # input placeholder for specific observation space
-            q_func=model,
-            num_actions=env.action_space.n,  # (1,)
-            optimizer=tf.train.AdamOptimizer(learning_rate=5e-4),
-            gamma = 0.99# relatively long-term
-        )
-        act_params = {
-        'make_obs_ph': lambda name: ObservationInput(env.observation_space, name=name),
-        'q_func': model,
-        'num_actions': env.action_space.n,
-        }
-
-        # act = deepq.deepq.ActWrapper(act, act_params)
-
-        replay_buffer = ReplayBuffer(50000) # Create the replay buffer, Max number of transitions to store in the buffer
-        # Schedule for exploration: 1 (every action is random) -> 0.02 (98% of actions selected by values predicted by model)
-        exploration = LinearSchedule(schedule_timesteps=5000, initial_p=1, final_p=1)
-        # exploration_start = 1
-        # exploration_end = 1 
-        # exploration = exploration_start
-        # action =  if np.random.uniform(0,1) > exploration  else env.action_space.sample()
-
-
-        # Initialize the parameters and copy them to the target network.
-        U.initialize()
-        update_target()
-
-        total_stepct = 0
-        episode_rewards = [] # each ele = reward from one complete run until done
-        actions_taken = []
-        for episode in range(max_episode_num): # each episode: restart run, keep learning on old weights
-            state = env.reset()  # returns state
-            episode_rewards.append(0)
-            
-            for step in range(steps_per_episode): # each step = 1 year, each episode = steps_per_episode years
-                total_stepct+=1
-                env.eco_step(99, delta = 0.01)
-                if step%25==0: print(f"   [{episode}-{step}] preaction: state={env.state}")
-                # Pick action and update exploration to the newest value
-                action = act(state[None], stochastic=False, update_eps=exploration.value(total_stepct))[0] # observation obj (axis added), stochastic boolean, update
-                new_state, rew, done, info = env.step(action)
-                actions_taken.append(action)
-                if step%25==0: print(f"   \tpostaction: state={env.state}, action={action}")
-                # Store transition in the replay buffer.
-                replay_buffer.add(state, action, rew, new_state, float(done))
-                state = new_state
-                episode_rewards[-1] += rew
-                if done: break # go to next episode
-                # is_solved = total_stepct > 100 and np.mean(episode_rewards[-101:-1]) >= 200
-                # if is_solved:
-                    # print(f"---SOLVED: {np.mean(episode_rewards[-101:-1])}---")
-                    # env.render()
-                else: 
-                    if total_stepct > -1: # Minimize the error in Bellman's equation on a batch sampled from replay buffer.
-                        obses_t, actions, rewards, obses_tp1, dones = replay_buffer.sample(32) # Sample a batch of experiences
-                        train(obses_t, actions, rewards, obses_tp1, dones, np.ones_like(rewards))
-            # end of each episode
-            update_target() # Update target network periodically.
-            logger.record_tabular("steps", total_stepct)
-            logger.record_tabular("episodes", len(episode_rewards))
-            logger.record_tabular("mean episode reward", round(np.mean(episode_rewards[-101:-1]), 1))
-            logger.record_tabular("% time spent exploring", int(100 * exploration.value(total_stepct)))
-            logger.dump_tabular()
-            
-            print(f"### episode {episode} ###: episode reward={episode_rewards[-1]} | State={env.state} | Total carbon={env.total_carbon()}\n")
-
-    
-        print(f"\n Actions taken: {actions_taken}; number of actions_taken={len(actions_taken)}")
-        act_fp = f"output/{time.time()}_act.pkl"
-        episode_rewards = np.array(episode_rewards)/steps_per_episode
-        print(f"\n Ending: reward={episode_rewards}\n ending state = {env.state}\n Act filepath = {act_fp}")
-
-        # act.save(act_fp)
-
-
-
-
-if __name__ == '__main__':
-    start_time = time.time()
-    parameters = {
-        "initial_state": np.array([150, 76, 0, 328, 328], dtype=np.float32), # soil_carbon, tree_carbon, product_carbon, oldtree_ct, youngtree_ct
-        "K_T": 0.008, # tree litterfall rate
-        "K_S": 1/50, # soil decay
-        "K_P": 1, # product decay
-        "m_1": 1/37, # product decay
-        "d_t" : 656, # tree density
-        "NPP_y": 2.5, # carbon released tons/ha/year for young trees
-        "NPP_o": 0.2, # carbon released tons/ha/year for old trees
-        "r_o":0.6,
-        "r_y":0.2,
-        "C_o": 0.076, # 76
-        "C_y": 0.038, # 38
-        "death_o": 0.05, 
-        "death_y": 0.1
-    }
-
-    # eco_run(parameters, undisturbed_iter=2000)
-    # dqn1(parameters, max_step_ct = 1000)
-    dqn2(parameters, max_episode_num = 20, steps_per_episode=100)
-    print("\n", parameters)
-    print("--- %s seconds ---" % (time.time() - start_time)) # round(time.time() - start_time, 2)
-    
-
-
-##### NOTES
-# act: function that choses an action given an observation
-# train: function that takes a transition (s,a,r,s') and optimizes Bellman equation's error:
-    # td_error = Q(s,a) - (r + gamma * max_a' Q(s', a'))
-    # loss = huber_loss[td_error]
-# https://github.com/openai/baselines/blob/ea25b9e8b234e6ee1bca43083f8f3cf974143998/baselines/deepq/build_graph.py#L317
